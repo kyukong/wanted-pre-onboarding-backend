74476ee8fea967cc1cdda002e7946285f3771b98
yukong <kyb062666@gmail.com>
2023-10-17 19:38:04 +0900

    feat: Job Entity 에 Company Entity 연관관계 선언

--- src/main/java/wanted/domain/Job.java
+++ src/main/java/wanted/domain/Job.java
@@ -1,10 +1,12 @@
 package wanted.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,9 @@ public class Job {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Company company;
 
 	private String position;
 	private int compensation;
