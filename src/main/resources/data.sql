7a396c5aef82face63fd54c0ebacd46997df8ca5
yukong <kyb062666@gmail.com>
2023-10-17 20:21:31 +0900

    chore: 회사, 사용자 테이블 DDL, DML 문 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+insert into users (name) values ("김티드");
+insert into users (name) values ("박티드");
+insert into users (name) values ("이티드");
+insert into users (name) values ("원티드");
+
+insert into company (name, country, region) values ("원티드랩", "한국", "서울");
+insert into company (name, country, region) values ("네이버", "한국", "판교");
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+alter table job drop foreign key FK5q04favsasq8y70bsei7wv8fc;
+
+drop table if exists company cascade;
+drop table if exists job cascade;
+drop table if exists users cascade;
+
+create table company (
+    id bigint not null auto_increment,
+    country varchar(255),
+    name varchar(255),
+    region varchar(255),
+    primary key (id)
+) engine=InnoDB;
+
+create table job (
+    compensation integer not null,
+    company_id bigint,
+    id bigint not null auto_increment,
+    description varchar(1000),
+    position varchar(255),
+    tech_stack varchar(255),
+    primary key (id)
+) engine=InnoDB;
+
+create table users (
+    id bigint not null auto_increment,
+    name varchar(255),
+    primary key (id)
+) engine=InnoDB;
+
+alter table job add constraint FK5q04favsasq8y70bsei7wv8fc
+   foreign key (company_id) references company (id);
