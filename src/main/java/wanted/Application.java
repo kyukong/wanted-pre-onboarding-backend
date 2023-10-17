80a452ead291ae356a53e6df28561b459678add8
yukong <kyb062666@gmail.com>
2023-10-17 19:15:33 +0900

    feat: 메인 Application 생성

---
+++ src/main/java/wanted/Application.java
@@ -0,0 +1,11 @@
+package wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
