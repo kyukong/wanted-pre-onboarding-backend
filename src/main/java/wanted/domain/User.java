6a1a7d051d23df54b2f4df4f78c72c398237e7e3
yukong <kyb062666@gmail.com>
2023-10-17 20:20:27 +0900

    refactor: 사용자 테이블명 변경 (user -> users)

--- src/main/java/wanted/domain/User.java
+++ src/main/java/wanted/domain/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User {
