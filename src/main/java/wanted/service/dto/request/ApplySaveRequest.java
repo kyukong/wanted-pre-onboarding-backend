a62c244ad615d807e8c4da572fa125b191fd07c4
yukong <kyb062666@gmail.com>
2023-10-18 21:19:31 +0900

    fix: 채용 공고 지원 Request DTO 에 생성자 추가

--- src/main/java/wanted/service/dto/request/ApplySaveRequest.java
+++ src/main/java/wanted/service/dto/request/ApplySaveRequest.java
@@ -1,9 +1,11 @@
 package wanted.service.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class ApplySaveRequest {
 
